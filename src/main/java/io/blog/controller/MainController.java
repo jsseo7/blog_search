4db83917caff15462798562226feb3674e94b93c
jsseo7 <chatnoir032747@gmail.com>
2023-03-18 19:17:22 +0900

    bootstrap table을 이용하여 데이터 입

---
+++ src/main/java/io/blog/controller/MainController.java
@@ -0,0 +1,18 @@
+package io.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+
+	@GetMapping("/main/blog")
+	public String blog() {
+		return "main/blog";
+	}
+}
